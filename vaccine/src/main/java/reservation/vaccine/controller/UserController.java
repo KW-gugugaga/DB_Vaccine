6a2d16d299ea44ba81422d5049eaf95dceb4e015
Yerim Hyun <gdf4013@naver.com>
2021-11-16 13:38:01 +0900

    login
    login

--- vaccine/src/main/java/reservation/vaccine/controller/UserController.java
+++ vaccine/src/main/java/reservation/vaccine/controller/UserController.java
@@ -1,15 +1,22 @@
 package reservation.vaccine.controller;
 
+import org.apache.ibatis.jdbc.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.tags.form.SelectTag;
 import reservation.vaccine.domain.UserInfo;
 import reservation.vaccine.service.UserService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Controller
 public class UserController {
@@ -18,11 +25,37 @@ public class UserController {
     UserService userService;
 
     @GetMapping("login")
-    public String Login(Model model) {
-        System.out.println("UserController.Login");
+    public String GetLogin(Model model) {
+        System.out.println("UserController.GetLogin");
         return "user/login";
-
     }
+
+    @PostMapping("login")
+    public String PostLogin(Model model, @RequestParam("ID") String ID, @RequestParam("PW") String PW) {
+        System.out.println("UserController.PostLogin");
+        System.out.println("ID : " + ID + ", PW : " + PW);
+        Integer Uid = userService.findUserById(ID);
+        if(Uid == null) {
+            System.out.println("아이디 존재하지 않음");
+            //팝업
+            return "user/login";
+        }
+        System.out.println("Uid = " + Uid);
+        String getPW = userService.findPWByUid(Uid);
+        System.out.println("getPW = " + getPW);
+        if(PW.equals(getPW)) {
+            System.out.println("비밀번호 일치");
+            String Uname = userService.findUnameByUid(Uid);
+            System.out.println("Uname = " + Uname);
+            model.addAttribute("Uname", Uname);
+            return "mainpage";
+        } else {
+            System.out.println("비밀번호 불일치");
+            //팝업
+            return "user/login";
+        }
+    }
+
     @GetMapping("join")
     public String join(Model model) {
         return "user/join";
@@ -44,7 +77,7 @@ public class UserController {
     public String findAll() {
         List<UserInfo> userInfos = userService.findAll();
         for(UserInfo userInfo : userInfos) {
-            System.out.println(userInfo.getUname());
+            System.out.println(userInfo.getUid() + " : " + userInfo.getUname());
         }
         return "mainpage";
     }
--- vaccine/src/main/java/reservation/vaccine/mapper/UserMapper.java
+++ vaccine/src/main/java/reservation/vaccine/mapper/UserMapper.java
@@ -2,9 +2,13 @@ package reservation.vaccine.mapper;
 
 import reservation.vaccine.domain.UserInfo;
 
+import java.util.HashMap;
 import java.util.List;
 
 public interface UserMapper {
 
     List<UserInfo> findAll();
+    Integer findUserById(String ID);
+    String findPWByUid(int Uid);
+    String findUnameByUid(int Uid);
 }
